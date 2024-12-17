19072bbe37e3eec853f6a4a19f4571e3f001ffa0
Alex Mateo <jdv.escobar@yavirac.edu.ec>
2024-12-17 16:37:15 -0500

    paginaaa

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PAGINA_WEB_ESCOBAR'`, () => {
+  it(`should have as title 'PAGINA_WEB`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PAGINA_WEB_ESCOBAR');
+    expect(app.title).toEqual('PAGINA_WEB');
   });
 
   it('should render title', () => {
